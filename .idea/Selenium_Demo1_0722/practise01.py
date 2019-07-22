07c99ce98309fb09cf62ea91b51a48f939f896c3
ILIFTWM <15675906534@163.com>
2019-07-23 00:12:40 +0800

    文件处理，异常，数据持久化

---
+++ .idea/Selenium_Demo1_0722/practise01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/22 23:52
+# @Author  : WM
+# @File    : practise01.py
+
+from selenium import webdriver
+driver  = webdriver.Firefox()
+driver.get("https://baidu.com")
